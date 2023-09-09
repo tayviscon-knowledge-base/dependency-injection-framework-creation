e243d0ea627c65e72abc83e6e77b6ac7a7cc1271
TyomychTovkach <tyomych.tovkach@tayviscon.ru>
2023-09-09 15:33:30 +0300

    Напиши Введение Доклада
    В рамках данного коммита описано введение доклада, а также проинициализирован проект exam-cheating-service.

---
+++ exam-cheating-service/src/main/java/ru/tayviscon/knowledge/base/service/ExamCheatingService.java
@@ -0,0 +1,26 @@
+package ru.tayviscon.knowledge.base.service;
+
+import ru.tayviscon.knowledge.base.domain.model.Student;
+import ru.tayviscon.knowledge.base.domain.model.Teacher;
+
+import java.util.List;
+
+/**
+ * Сервис, помогающий студентам списывать на экзамене.
+ *
+ * @author Tyomych Tovkach
+ */
+public class ExamCheatingService {
+
+    /**
+     * Основной метод сервиса.
+     *
+     * @param teacher  преподаватель, которого необходимо отвлечь.
+     * @param students студенты, которым необходимо разослать ответы.
+     */
+    public void start(Teacher teacher, List<Student> students) {
+        // todo отвлечь преподавателя
+        // todo разослать студентам ответы
+    }
+
+}
